ba77faf7fa4149806df5cdc69788af16b8f204a7
Yulia-Yakovleva <st041958@student.spbu.ru>
2019-10-02 21:24:31 +0300

    Добавлен скрипт и табличка с результатом его работы

---
+++ README.md
@@ -0,0 +1,4 @@
+Project is here!
+
+Здесь лежит скрипт для парсинга NCBI Assembly: assembly_parsing.py
+И результат его работы: assembly_stats.txt
---
+++ assembly_stats.py
@@ -0,0 +1,42 @@
+#/usr/bin/python    
+
+from Bio import Entrez
+Entrez.email = "mike.rayko@gmail.com" 
+handle = Entrez.esearch(db="assembly", term="metagenome",retmax = 10500) #10500)
+record = Entrez.read(handle)
+print ("got assembly ids")
+
+id_list = record["IdList"]
+print len(id_list)
+i = 0
+
+spl = []
+spl.append(id_list[:5000])
+spl.append(id_list [5000:])
+sample_list=[]
+
+for id_l in spl:
+    joined=",".join(id_l)
+    handle = Entrez.esummary(db="assembly", id=joined, retmax = 10500)
+    entry = Entrez.read(handle)
+    elems = entry["DocumentSummarySet"]["DocumentSummary"]
+    print ("got summaries")
+    print (len(elems))
+    for elem in elems:
+        i += 1
+        if i % 500 == 0:
+            print i
+        taxid = elem["Taxid"]
+        gbid =elem["GbUid"]
+        acc = elem["AssemblyAccession"]
+        taxid = elem["Taxid"]
+        orgn = elem["Organism"]
+        sptaxid = elem["SpeciesTaxid"]
+        spname = elem["SpeciesName"]
+        size = elem["Meta"].split("</Stat>")[14].split(">")[-1]
+    
+        sample_list.append({"gbid":gbid, "acc":acc,"taxid":taxid,"orgn":orgn,"sptaxid":sptaxid,"spname":spname,"size":size})
+    
+with open('assembly_stats.txt', 'w') as f:
+    for item in sample_list:
+        f.write("%s\n" % item)
