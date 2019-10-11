d5248fe536de005afa0f361420fc44cb52305cda
oxygen311 <alex39-09@mail.ru>
2019-10-11 18:01:21 +0300

    seawater metagenome: plasmid data,part 1

--- viralverify_runner.sh
+++ viralverify_runner.sh
@@ -3,5 +3,5 @@ folder='seawater_metagenome'
 
 for file in `find $folder -name "*_genomic_circular_only.fna"`; do
     dir_path=`dirname "$file"`
-    python ~/viralVerify/viralverify.py -f ./${file} -o ./${dir_path}/viralverify_output --hmm ~/viralVerify/nbc_hmms.hmm -t 4
+    python ~/viralVerify/viralverify.py -f ./${file} -o ./${dir_path}/viralverify_output --hmm ~/viralVerify/nbc_hmms.hmm -t 4 -p
 done
