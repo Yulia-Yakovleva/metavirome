7e5ccf0faef671f5eae0a4f35fd55e0da049a5fc
ArtyomKaltovich <KaltovichArtyom@gmail.com>
2019-12-09 21:31:33 +0300

    update map

--- map/draw_map.py
+++ map/draw_map.py
@@ -1,12 +1,14 @@
 import folium
 import pandas as pd
+from folium.plugins import MarkerCluster
 
 data = pd.read_csv(r"coordinates.csv", na_values=["None"])
 data = data.dropna()
 
 m = folium.Map()
+cluster = MarkerCluster().add_to(m)
 
 for d in data.itertuples():
-    folium.Marker([d.latitude, d.longitude], popup=d.id, tooltip=d.id2).add_to(m)
+    folium.Marker([d.latitude, d.longitude], popup=d.id, tooltip=d.id2).add_to(cluster)
 
 m.save("map.html")
